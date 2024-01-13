8b530758c19228ece1465577cfe8018bb48d1757
JohnStrangeman <istranj06@gmail.com>
2024-01-13 12:36:53 +0100

    Added the isntall script (probably doesn't work)

---
+++ installDependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -d ~/Clone ]
+then
+    echo "~/Clone directory exists..."
+    rm -rf ~/Clone/
+fi
+
+mv gtk-2.0 gtk-3.0 kitty neofetch qtile rofi ~/.config/
+
+mkdir ~/Clone
+git clone https://aur.archlinux.org/yay-bin.git ~/Clone/
+cd ~/Clone/yay-bin/
+makepkg -si
+
+yay -Syy
+
+yay -S gtk3 gtk2 kitty neofetch qtile qtile-extras rofi python thunar --needed --noconfirm
