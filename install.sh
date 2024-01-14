6eaed778ac393a10927eccd83211eb625a8de96c
JohnStrangeman <istranj06@gmail.com>
2024-01-14 12:51:08 +0100

    Changing order of installation

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+clonePath = ${pwd}
 if [ -d ~/Clone ]
 then
     echo "~/Clone directory exists..."
     rm -rf ~/Clone/
 fi
-mv gtk-2.0 gtk-3.0 kitty neofetch qtile rofi ~/.config/
-mv .zshrc .bashrc ~/
 
 mkdir -p ~/Clone/yay-bin
 git clone https://aur.archlinux.org/yay-bin.git ~/Clone/yay-bin
@@ -20,6 +19,9 @@ systemctl enable lightdm.service
 cd $ZSH_CUSTOM/plugins && git clone https://github.com/chrissicool/zsh-256color
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cd $clonePath
+mv .zshrc .bashrc ~/
+mv gtk-2.0 gtk-3.0 kitty neofetch qtile rofi ~/.config/
 systemctl start lightdm.service
 echo "greeter-session=lightdm-gtk3-greeter" > /etc/lightdm/lightdm.conf
 
