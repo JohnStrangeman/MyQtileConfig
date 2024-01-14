6d50664890e39fdd45f4c6811a495177ec354851
JohnStrangeman <istranj06@gmail.com>
2024-01-14 12:10:41 +0100

    Adding .zshrc and .bashrc and changing name to install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ then
     echo "~/Clone directory exists..."
     rm -rf ~/Clone/
 fi
-
 mv gtk-2.0 gtk-3.0 kitty neofetch qtile rofi ~/.config/
+mv .zshrc .bashrc ~/
 
-mkdir ~/Clone
-git clone https://aur.archlinux.org/yay-bin.git ~/Clone/
+mkdir -p ~/Clone/yay-bin
+git clone https://aur.archlinux.org/yay-bin.git ~/Clone/yay-bin
 cd ~/Clone/yay-bin/
 makepkg -si
 
