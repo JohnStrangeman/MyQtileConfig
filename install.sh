7337a30f9ac4e311da34a4d83aa8e90194299793
JohnStrangeman <istranj06@gmail.com>
2024-01-14 12:39:53 +0100

    Adding lightdm installation because it turns out I am an idiot who forgot it

--- install.sh
+++ install.sh
@@ -15,7 +15,12 @@ makepkg -si
 
 yay -Syy
 
-yay -S gtk3 gtk2 kitty neofetch qtile qtile-extras rofi python eza oh-my-zsh-git zsh-theme-powerlevel10k-git pokemon-colorscripts-git --needed --noconfirm
+yay -S gtk3 gtk2 kitty neofetch qtile qtile-extras lightdm lightdm-gtk-greeter rofi python eza oh-my-zsh-git zsh-theme-powerlevel10k-git pokemon-colorscripts-git --needed --noconfirm
+systemctl enable lightdm.service 
 cd $ZSH_CUSTOM/plugins && git clone https://github.com/chrissicool/zsh-256color
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+systemctl start lightdm.service
+echo "greeter-session=lightdm-gtk3-greeter" > /etc/lightdm/lightdm.conf
+
+echo "Reboot the pc after instalation using reboot command"
