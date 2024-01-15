355b6b3a784f5185fe29f2059d629a218857e6be
JohnStrangeman <istranj06@gmail.com>
2024-01-15 16:59:32 +0100

    adding libvirted and NvChad install

--- install.sh
+++ install.sh
@@ -16,13 +16,26 @@ yay -Syy
 
 yay -S gtk3 gtk2 kitty neofetch qtile qtile-extras lightdm lightdm-gtk-greeter rofi python eza oh-my-zsh-git zsh-theme-powerlevel10k-git pokemon-colorscripts-git --needed --noconfirm
 systemctl enable lightdm.service 
+
+mkdir $ZSH_CUSTOM/plugins
 cd $ZSH_CUSTOM/plugins && git clone https://github.com/chrissicool/zsh-256color
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 cd $clonePath
 mv .zshrc .bashrc ~/
 mv gtk-2.0 gtk-3.0 kitty neofetch qtile rofi ~/.config/
-systemctl start lightdm.service
-echo "greeter-session=lightdm-gtk3-greeter" > /etc/lightdm/lightdm.conf
 
-echo "Reboot the pc after instalation using reboot command"
+yay -S neovim
+git clone https://github.com/NvChad/NvChad.git --depth 1 ~/.config/nvim/
+
+yay -S qemu-desktop virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat --needed --noconfirm
+yay -S ebtables iptables --needed --noconfirm
+
+systemctl enable libvirtd.service
+systemctl start libvirtd.service
+usermod -a -G libvirt $(whoami)
+systemctl restart libvirtd.service
+
+echo "greeter-session=lightdm-gtk3-greeter" > /etc/lightdm/lightdm.conf
+systemctl start lightdm.service
