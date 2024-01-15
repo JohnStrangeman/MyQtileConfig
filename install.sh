2141d8c000f59e2cff1f0dc9a106d59efa094a80
JohnStrangeman <istranj06@gmail.com>
2024-01-15 17:42:21 +0100

    Added the wallpaper folder with all the wallpapers as I just noticed I forgot that one

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 cd $clonePath
-mv .zshrc .bashrc .icons ~/
+mv .zshrc .bashrc .icons wallpapers ~/
 mv gtk-2.0 gtk-3.0 kitty neofetch qtile rofi ~/.config/
 mv fonts/ ~/.local/share/
 mv wal ~/.cache/
